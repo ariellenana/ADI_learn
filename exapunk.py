16f95221a49ad863e7a0294521e5b86f80e06c8d
Arielle Na <nana.arielle@gmail.com>
2022-02-03 22:47:03 -0800

    Add files via upload
    read line function is working

--- exapunk.py
+++ exapunk.py
@@ -6,31 +6,37 @@ class exa:
         self.x = 0
         self.t = 0
     
-
-
-    def read_lines(lines):
+    
+    
+    def read_lines(self, lines):
         # ignore empty lines
-        if lines == None:
+        if lines == '': # None? 
             pass
         else:
-            exp = list(lines.strip().split())[0], list(lines.strip().split())[1:]
-            #b = list(lines.strip().split())
-            #exp = b[0], b[1:]
-            for item in exp[1:]:
-                if item not in ['X, T'] or not str(item).isdigit():
-                    print('Invalid code')
-                    break
-                if str(item).isdigit() == True:
-                    item = int(item)
-                    print(item)
-                    print(type(item))
-            return exp
+            reading = list(lines.strip().split())
+            arg_new = []
+            instruction_list = list('COPY ADDI SUBI MULI DIVI MODI'.split())
+            if reading[0] not in instruction_list:
+                print('Invalide instruction.  CRASH  !!!')
+                pass
+            else:
+                for item in reading[1:]:
+                    #print('item is: ', item)
+                    if item not in ['X', 'T'] and not str(item).isdigit():
+                        print('Invalid code! CRASH!!!')
+                        break
+                    if str(item).isdigit() is True:
+                        item = int(item)
+                    arg_new.append(item)
+                    #print('new list is: ', arg_new)
+                exp = reading[0], arg_new
+                return exp
 
     def func_copy(self, value, reg):
         if reg == 'X':
             self.x = value
             return self.x 
-        if reg == 'T':
+        elif reg == 'T':
             self.t = value
             return self.t 
         else:
